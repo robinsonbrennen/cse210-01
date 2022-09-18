d88b450e328833cf785b04094316d457c35c391b
robinsonbrennen <rob16073@byui.edu>
2022-09-17 20:45:59 -0600

    finishing touches

--- ticTacToe.py
+++ ticTacToe.py
@@ -1,9 +1,19 @@
+"""
+Author: Brennen Robinson
+W01 Prove: Developer
+Date: 09/17/2022
+"""
+
+
 game_over = False
+
 players = [
     (1, 'X'),
     (2, 'O')
 ]
+
 player = players[0]
+
 points = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 while True:
@@ -21,21 +31,28 @@ while True:
     point = int(number) - 1
     points[point] = player[1]
 
-    # Horizontal
+    # Iterate through next 3 sections to determine if game is over
+
+    # Side to side
+
     if points[0] == points[1] == points[2]:
         game_over = True
     elif points[3] == points[4] == points[5]:
         game_over = True
     elif points[6] == points[7] == points[8]:
         game_over = True
-    # Vertical
+
+    # Up and down
+
     elif points[0] == points[3] == points[6]:
         game_over = True
     elif points[1] == points[4] == points[7]:
         game_over = True
     elif points[2] == points[5] == points[8]:
         game_over = True
+
     # Diagonal
+
     elif points[0] == points[4] == points[8]:
         game_over = True
     elif points[2] == points[4] == points[6]:
